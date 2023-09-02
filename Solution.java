b4e65c6a5afc239a6272cea32fcaa5a2913000d7
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-01 18:18:59 -0700

    first attempt

--- Solution.java
+++ Solution.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 public class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> answer = new ArrayList<>();
+        int max_candies = Integer.MIN_VALUE;
+
+        for (int candy : candies) {
+            if (candy > max_candies)
+                max_candies = candy;
+        }
+
+        for (int candy : candies) {
+            answer.add(candy + extraCandies >= max_candies);
+        }
         
-        return new ArrayList<>();
+        return answer;
     }
 }
